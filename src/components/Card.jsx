4359735cee5f0703d2e99b87b9aab8408b807028
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-02-07 21:14:06 -0300

    update Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
+import CardTemp from "./CardTemp";
 
 export default function Card({max, min, name, img, onClose}) {
     // acá va tu código
     function handleOnclose() {
         if (typeof onClose === 'function') onClose();
     }
+
     return (
         <div>
             <button onClick={handleOnclose}>X</button>
             <span>{name}</span>
-            <div>
-                <span>Min</span>
-                <span>{min}</span>
-            </div>
-            <div>
-                <span>Max</span>
-                <span>{max}</span>
-            </div>
+            <CardTemp label='Min' value={min}/>
+            <CardTemp label='Max' value={max}/>
             <div>
                 <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt=""/>
             </div>
